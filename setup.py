80782be8e80a7a8845dbb478a4823837d018df1c
jof2jc <jof2jc@gmail.com>
2021-04-02 08:35:39 +0000

    setup.py lazop

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
+
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
-
-# get version from __version__ variable in lazop/__init__.py
-from lazop import __version__ as version
-
 setup(
-	name='lazop',
-	version=version,
-	description='Lazada SDK',
-	author='Lazada',
-	author_email='jof2jc@gmail.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name='lazop-sdk-python',
+    version='1.1.0',
+    author='top',
+    author_email='xuteng.xt@alibaba-inc.com',
+    packages=find_packages(),
+    install_requires=[], #install_requires
+    license='MIT',
 )
+
